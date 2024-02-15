bf126570ba5513546feed045920d7da1519ecfa7
brolaa <bartosz.rola@gmail.com>
2024-02-15 19:21:55 +0100

    Project 1 files added

---
+++ 01-WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Bartosz Rola on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
