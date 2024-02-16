c4731b0736806d64f9564bcf03cade5792fba826
brolaa <bartosz.rola@gmail.com>
2024-02-16 13:20:17 +0100

    Project 7 added

---
+++ 07-WordScramble/README.md
@@ -0,0 +1,4 @@
+# Project 5 - WordScramble
+A game that show players a random eight-letter word, and asks them to make words out of it.
+### Screenshots
+![Screenshot 7](./Screenshots/screenshot07.gif)
---
+++ 07-WordScramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Bartosz Rola on 02/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
